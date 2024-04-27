bae072d4ffac7037a73f8cb4b89b9da5df71a77f
Zhoss <joss.90@mail.ru>
2024-04-27 10:35:32 +1000

    Task1: return string "computer" in the required declension depending on the number.

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,33 @@ public class Main {
     public static void main(String[] args) {
 
     }
+
+    public static String computerNumber(int number) {
+        int ones = number % 10;
+        int tens = number % 100;
+
+        String result = number + " компьютер";
+        String ending;
+
+        switch (ones) {
+            case 1 -> {
+                if (tens == 11) {
+                    ending = "ов";
+                } else {
+                    ending = "";
+                }
+            }
+            case 2, 3, 4 -> {
+                if (tens == 12 || tens == 13 || tens == 14) {
+                    ending = "ов";
+                } else {
+                    ending = "а";
+                }
+            }
+            default -> ending = "ов";
+        }
+
+        return result + ending;
+    }
 }
 
